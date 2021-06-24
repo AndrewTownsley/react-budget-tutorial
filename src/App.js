a6041d68e2560c61e934613c91ba0ea9a88c8f38
Andrew TOwnsley <atownsley86@gmail.com>
2021-06-23 22:29:00 -0600

    adding the addContext api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { useState } from 'react';
+import { AppProvider } from './context/AppContext';
 //Components
 import Item from "./Components/Item"
 import Budget from "./Components/Budget"
@@ -11,6 +12,7 @@ import AddExpense from "./Components/AddExpense"
 
 function App() {
   return (
+    <AppProvider>
     <div className="container">
       <h1 className="mt-3">Budget App</h1>
       <div className="row mt-3">
@@ -23,10 +25,21 @@ function App() {
         <div className="col-sm">
           <Spent/>
         </div>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList/>
+          </div>
+        </div>
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AddExpense/>
+          </div>
+        </div>
       </div>  
-      <ExpenseList/>
-      <AddExpense/>
     </div>
+    </AppProvider>
   );
 }
 
--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -2,12 +2,33 @@
 const AddExpense = () => {
 
   return (
-    <div className="alert alert-secondary">
-      <label htmlFor="">Add Expense</label>
-      <input type="text" />
-      <label htmlFor="">Expense Cost</label>
-      <input type="text" />
-    </div>
+    <form action="">
+      <div className="row">
+        <div className="col-sm">
+          <label htmlFor="name">name</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+          />
+        </div>
+        <div className="col-sm">
+          <label htmlFor="cost">cost</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="cost"
+          />
+        </div>
+      </div>
+        <div className="row">
+          <div className="col-sm">
+            <button className="btn btn-primary mt-3">Save</button>
+          </div>
+        </div>
+    </form>
   )
 }
 
